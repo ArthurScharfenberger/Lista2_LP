7ee59a343cf8ff04365e2c66ea7c9ec476cef5d0
Arthur Scharfenberger <64555332+ArthurScharfenberger@users.noreply.github.com>
2026-03-30 21:05:48 -0300

    10

--- exer10.py
+++ exer10.py
@@ -4,7 +4,9 @@ litros = area / 3
 latas = litros / 18 + 1
 preco = int(latas) * 80
 
+if litros % 18 == 0:
+    latas = litros / 18
+    preco = int(latas) * 80
+
 print(f"Quantidade de latas: {int(latas)}")
 print(f"Preço total: R$ {preco:.2f}")
-
-#Como a quantidade de latas pode dar um valor quebrado, foi adicionada 1 lata.
